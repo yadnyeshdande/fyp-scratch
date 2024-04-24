3e69c43da66daac0beaa4fc86d9669f253d766ab
Yadnyesh <yrdande_b21@el.vjti.ac.in>
2024-04-25 00:50:14 +0530

    gallery updated

--- src/components/Hgallery.tsx
+++ src/components/Hgallery.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 function Hgallery() {
   return (
-    <div className='hgallery px-40 h-screen'>
+    <div className='hgallery px-40 h-auto'>
 
     <div className=" font-bold flex flex-col ">
         <div className="font-black font-sans text-7xl flex  gap-5 text-white text-stroke text-cyan-indigo-600 text-center mt-5">
@@ -13,37 +13,40 @@ function Hgallery() {
         </div>
         <h3 className=' mx-10 my-8 mt-6'> ✪ Clean rooms with discipline. <br/> ✪ Clean Mess with veg and non-veg meal availble for students. <br /> ✪ Supports Events and Festivals.</h3>
         </div> 
-        <div className=' flex flex-row mt-4'>
+        <div className=' flex flex-row gap-1 mt-4'>
             <Image
-            src="/"
+            src="/vjtiDroneShot.jpeg"
             alt="Picture of the author"
             width={500}
             height={500}></Image>
 
             <Image
-            src="/"
+            src="/vjtiTopView.jpeg"
             alt="Picture of the author"
             width={500}
             height={500}></Image>
 
             <Image
-            src="/"
+            src="/quad.jpeg"
             alt="Picture of the author"
             width={500}
             height={500}></Image>
 
-            <Image
-            src="/"
-            alt="Picture of the author"
-            width={500}
-            height={500}></Image>
-
-            <Image
-            src="/"
-            alt="Picture of the author"
-            width={500}
-            height={500}></Image>
         </div>
+        <div className=' flex  flex-row gap-1 mt-4 mb-4 justify-center items-center'>
+            <Image
+            src="/hostel.png"
+            alt="Picture of the author"
+            width={500}
+            height={500}></Image>
+
+            <Image
+            src="/hostelCricket.png"
+            alt="Picture of the author"
+            width={500}
+            height={500}></Image>
+          </div>
+
     </div>
   )
 }
